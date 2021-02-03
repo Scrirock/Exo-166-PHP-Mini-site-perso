a3b513863e23d7368c659a725e4d611aa971d7ec
Scrirock <73163680+Scrirock@users.noreply.github.com>
2021-02-03 15:32:50 +0100

    point 5

--- pages/contact.php
+++ pages/contact.php
@@ -1,3 +1,10 @@
 <section>
     <h1>Contact</h1>
 </section>
+<section>
+    <form action="../public/save.php" method="POST">
+        <label for="name">Nom: </label>
+        <input type="text" name="name" id="name">
+        <input type="submit">
+    </form>
+</section>
